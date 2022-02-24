1265a478c92e05e985228b3852ae9eff9b054642
Laravel Daily <info@laraveldaily.com>
2022-02-24 03:37:31 +0000

    QuickAdminPanel automatic commit

--- database/migrations/2022_02_24_000007_create_lantais_table.php
+++ database/migrations/2022_02_24_000007_create_lantais_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRuangsTable extends Migration
+class CreateLantaisTable extends Migration
 {
     public function up()
     {
-        Schema::create('ruangs', function (Blueprint $table) {
+        Schema::create('lantais', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->integer('kapasitas');
+            $table->string('slug')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
--- database/migrations/2022_02_24_000008_create_ruangs_table.php
+++ database/migrations/2022_02_24_000008_create_ruangs_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLantaisTable extends Migration
+class CreateRuangsTable extends Migration
 {
     public function up()
     {
-        Schema::create('lantais', function (Blueprint $table) {
+        Schema::create('ruangs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->integer('kapasitas');
+            $table->string('slug')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
